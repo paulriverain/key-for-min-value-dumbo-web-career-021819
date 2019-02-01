c58ea51b54ba5ca78e1807b03bbf0d4df210f08f
github username <github email address>
2019-02-01 16:13:03 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,15 @@
 
 def key_for_min_value(name_hash)
   min_v = nil
-  min_k = nil
+  min_k = nil #=>set both variables = to nil
 name_hash.each do |key, val|
-  if min_v == nil
+  if min_v == nil #if min_v = nil return nil for both mins
     min_v = val
     min_k = key
-  elsif min_v > val
+  elsif min_v > val #if value is less than min_v, set variables
     min_v = val
     min_k = key
   end
 end
-min_k
+min_k #return the key for the smallest value
 end
